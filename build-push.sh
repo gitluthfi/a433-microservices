868c2f40eb49dc6da86d04b90215bbd9763cdaea
gitluthfi <rafiluthfi247@gmail.com>
2023-06-10 23:32:32 +0700

    update image v1

--- build-push.sh
+++ build-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Perintah untuk membuat Docker image
-docker build -t luthfi247/order-service:latest .
+docker build -t luthfi247/order-service:v1 .
 
 # Melihat daftar image di lokal
 docker images
@@ -10,4 +10,4 @@ docker images
 docker login
 
 # Mengunggah image ke Docker Hub
-docker push luthfi247/order-service:latest
+docker push luthfi247/order-service:v1
